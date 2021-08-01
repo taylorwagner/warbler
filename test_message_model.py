4035232c60dfc18656543d190951b0f916fb5d91
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-08-01 15:13:40 -0500

    set up and tear down for message model

--- test_message_model.py
+++ test_message_model.py
@@ -1,3 +1,4 @@
+"""Message model tests."""
 import os
 from unittest import TestCase
 from sqlalchemy import exc
@@ -18,17 +19,14 @@ class MessageModelTestCase(TestCase):
         db.drop_all()
         db.create_all()
 
-        u1 = User.signup("test1", "email1@email.com", "password", None)
-        uid1 = 1111
-        u1.id = uid1
-
-        u2 = User.signup("test2", "email2@email.com", "password", None)
-        uid2 = 2222
-        u2.id = uid2
-
+        self.uid = 1017
+        u = User.signup("test1", "email1@email.com", "password", None)
+        u.id = self.uid
         db.session.commit()
 
-        m1 = Message.
+        self.u = User.query.get(self.uid)
+
+        self.client = app.test_client()
 
     def tearDown(self):
         res = super().tearDown()
